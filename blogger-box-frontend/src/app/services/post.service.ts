d956ba0dfa20c70ed99048f39a45c42a9d579c5b
Georgio Azar <georgio.azar@dauphine.eu>
2026-04-11 14:09:21 +0200

    Update PostService to use environment variable for API URL in getPosts method

--- blogger-box-frontend/src/app/services/post.service.ts
+++ blogger-box-frontend/src/app/services/post.service.ts
@@ -2,14 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Post } from '../data/post';
+import { environment } from '../environment/environment';
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class PostService {
   constructor(private http: HttpClient) {}
 
   getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>('/api/posts');
+    return this.http.get<Post[]>(`${environment.apiUrl}v1/posts`);
   }
 }

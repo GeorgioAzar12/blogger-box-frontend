3027b9a3732cfddd8a0676fea277a2f48a20b60f
Georgio Azar <georgio.azar@dauphine.eu>
2026-04-11 18:22:31 +0200

    Add AddPost component with form for creating new posts and update routing

--- blogger-box-frontend/src/app/app-module.ts
+++ blogger-box-frontend/src/app/app-module.ts
@@ -8,9 +8,10 @@ import { provideHttpClient } from '@angular/common/http';
 import { PostList } from './components/post-list/post-list';
 import { PostListItem } from './components/post-list-item/post-list-item';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AddPost } from './components/add-post/add-post';
 
 @NgModule({
-  declarations: [App, TopBar, PostList, PostListItem],
+  declarations: [App, TopBar, PostList, PostListItem, AddPost],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [provideBrowserGlobalErrorListeners(), provideHttpClient()],
   bootstrap: [App],

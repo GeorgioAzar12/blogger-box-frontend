322ff741ddc1ef3bcdbf8207a3eee1ca705c9bab
Georgio Azar <georgio.azar@dauphine.eu>
2026-04-11 17:41:29 +0200

    Add CategoryService with getCategories method for category retrieval

---
+++ blogger-box-frontend/src/app/services/category.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environment/environment.prod';
+import { Category } from '../data/category';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class CategoryService {
+  constructor(private http: HttpClient) {}
+
+  getCategories() {
+    return this.http.get<Category[]>(`${environment.apiUrl}v1/categories`);
+  }
+}
